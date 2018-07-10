07f4ead09c5fb725ee0159a2a2822100ad4c16b1
kilometro28 <venturaguc@gmail.com>
2018-07-10 20:25:52 +0200

    version 1.0

---
+++ a.rb
@@ -0,0 +1,29 @@
+require 'gosu'
+
+class WhackARuby < Gosu::Window
+  def initialize
+    super(800, 600)
+    self.caption = "Whack a ruby!"
+    @image = Gosu::Image.new('img\rubys.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
+  end
+  
+  def update()
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+  end
+
+  def draw()
+    @image.draw(@x - @width / 2, @y - @height / 2, 1)
+  end
+end
+
+window = WhackARuby.new
+window.show

3cce22953236c10b61f07d102c582e543bd46e5b
kilometro28 <venturaguc@gmail.com>
2018-07-10 22:02:42 +0200

    new variable added

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -11,17 +11,22 @@ class WhackARuby < Gosu::Window
     @height = 43
     @velocity_x = 5
     @velocity_y = 5
+    @visible = 0
   end
-  
+
   def update()
     @x += @velocity_x
     @y += @velocity_y
     @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
     @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
 
   def draw()
-    @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    end
   end
 end
 

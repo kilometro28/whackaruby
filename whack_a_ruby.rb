7e6493b77be2a3e9db621fb8730a2e533c3a3b86
kilometro28 <venturaguc@gmail.com>
2018-07-11 18:44:19 +0200

    final version

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -2,7 +2,7 @@ require 'gosu'
 
 class WhackARuby < Gosu::Window
   def initialize
-    super(1080, 720)
+    super(800, 600)
     self.caption = "Whack a ruby!"
     @image = Gosu::Image.new('img\rubys.png')
     @x = 200
@@ -14,15 +14,23 @@ class WhackARuby < Gosu::Window
     @visible = 0
     @hammer = Gosu::Image.new('img\hammers.png')
     @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
+    @playing = true
+    @start_time = 0
   end
 
   def update()
-    @x += @velocity_x
-    @y += @velocity_y
-    @velocity_x *= -1 if @x + @width / 2 > 1080 || @x - @width / 2 < 0
-    @velocity_y *= -1 if @y + @height / 2 > 720 || @y - @height / 2 < 0
-    @visible -= 1
-    @visible = 30 if @visible < -10 && rand < 0.01
+    if @playing
+      @x += @velocity_x
+      @y += @velocity_y
+      @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+      @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+      @visible -= 1
+      @visible = 30 if @visible < -10 && rand < 0.01
+      @time_left = (100 - ((Gosu.milliseconds - @start_time) / 1000))
+      @playing = false if @time_left < 0
+    end
   end
 
   def draw()
@@ -33,24 +41,42 @@ class WhackARuby < Gosu::Window
     if @hit == 0
       c = Gosu::Color::NONE
     elsif @hit == 1
-      c = Gosu::Color::RED
-    elsif @hit == -1
       c = Gosu::Color::GREEN
+    elsif @hit == -1
+      c = Gosu::Color::RED
     end
-    draw_quad(0, 0, c, 1080, 0, c, 1080, 720, c, 0, 720, c)
+    draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
     @hit = 0
+    @font.draw(@time_left.to_s, 20, 20, 2)
+    @font.draw(@score.to_s, 700, 20, 2)
+    unless @playing
+      @font.draw("Game over", 300, 300, 3)
+      @font.draw("Press the Space Bar to Play Again", 175, 350, 3)
+      @visible = 20
+    end
   end
 
   def button_down(id)
-    if (id == Gosu::MsLeft)
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        @hit = 1
-      else
-        @hit = -1
+    if @playing  
+      if (id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          @hit = 1
+          @score += 5 
+        else
+          @hit = -1
+          @score -= 1
+        end
       end
+    else
+      if (id == Gosu::KbSpace)
+        @playing = true
+        @visible = -10
+        @start_time = Gosu.milliseconds
+        @score = 0
     end
   end
 end
 
 window = WhackARuby.new
 window.show
+end
